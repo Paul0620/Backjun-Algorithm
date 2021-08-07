53c468bdab38843a18b66fe08705817f49d89094
Paul0620 <dlrhkdghsp@gmail.com>
2021-08-07 22:59:24 +0900

    Update 2217.py

--- greedy/2217.py
+++ greedy/2217.py
@@ -8,3 +8,21 @@ for _ in range(n):
 
 result = min(arr)
 print(result * n)
+
+
+"""
+#로프
+n = int(input())
+
+ropes = []
+result = []
+
+for _ in range(n):
+  ropes.append(int(input()))
+ropes.sort(reverse=True)
+
+for j in range(n):
+  result.append(ropes[j] * (j + 1))
+
+print(max(result))
+"""
