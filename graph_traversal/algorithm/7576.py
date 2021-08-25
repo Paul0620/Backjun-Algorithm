29f23ceb60d94d9121288fc962e471b2e7e5ca12
Paul <dlrhkdghsp@gmail.com>
2021-08-25 18:16:21 +0900

    Update 7576.py

--- graph_traversal/algorithm/7576.py
+++ graph_traversal/algorithm/7576.py
@@ -2,78 +2,57 @@
 from sys import stdin
 from collections import deque
 
-def bfs(x, y):
-    que = deque([(x, y)])
-    global result
+# BFS를 이용해 토마토가 며칠만에 다 익는지 확인한다
+
+
+def bfs(que):
+    global days
     while que:
-        x, y = que.popleft()
+        # 시간복잡도가 O(1)인 deque의 popleft()함수를 이용하여 첫번째 값을 가져온다.
+        x, y, depth = que.popleft()
+        # 시작값 주변에 찾기
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or x < nx or ny < 0 or y < ny:
-                continue
-            if graph[nx][ny] == -1:
-                continue
-            if graph[nx][ny] == 0:
-                result += 1
-                graph[nx][ny] = graph[x][y] + 1
-                que.append((nx, ny))
-    return graph[n - 1][m - 1]
+            # 범위 안에 있다면
+            if 0 <= nx < n and 0 <= ny < m:
+                # 주변에 0(안익은 토마토)가 있다면 1로 바꾸고 날짜를 1증가
+                if graph[nx][ny] == 0:
+                    graph[nx][ny] = 1
+                    que.append([nx, ny, depth + 1])
+                    days = depth + 1
+    return days
+
 
 m, n = map(int, stdin.readline().split())
 graph = [list(map(int, stdin.readline().split())) for _ in range(n)]
+que = deque()
+# 상, 하, 좌, 우를 위한 값
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-cnt = 0
-result = 0
+# 날짜 초기값 설정
+days = 0
+# 안익은 토마토 체크를 위한 값 설정
+check = 0
 
-# 0이 하나도 없고 1이 한개 이상 있다면 -> 0 출력
-# -1이 있거나 없거나 길이 있고 최종 좌표까지 갈 수 있다면 모든 0을 1로 바꾸는데 걸리는 카운트 수 출력
-
-# -1로 인해 길이 막혀 0을 1로 바꿀 수 없다면 -> -1 출력
-for i in graph:
-    if 0 not in i:
-        cnt += 1
-if cnt == len(graph):
-    print(-1)
-
-#for i in range(n):
- #   for j in range(m):
-
-  #      if graph[i][j] ==
-
-
-
-from sys import stdin
-from collections import deque
-n, m = map(int, stdin.readline().split())
-arr = [list(map(int, stdin.readline().split())) for _ in range(m)]
-checker = 1
-cnt = 0
-di = [-1, 0, 1, 0]
-dj = [0, 1, 0, -1]
-que = deque()
-for i in range(m):
-    for j in range(n):
-        if arr[i][j] == 1:
+# deque함수를 이용하여 que에 익은 토마토를 찾아 그 위치와 시작값을 담는다
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
             que.append([i, j, 0])
-answer = 0
-while que:
-    i, j, depth = que.popleft()
-    for a in range(4):
-        ni = i + di[a]
-        nj = j + dj[a]
-        if 0 <= ni < m and 0 <= nj < n:
-            if arr[ni][nj] == 0:
-                arr[ni][nj] = 1
-                que.append([ni, nj, depth+1])
-                answer = depth + 1
-in_zero = 0
-for line in arr:
-    if 0 in line:
-        in_zero = 1
+
+# BFS에 que에 담은 값을 보낸다
+bfs(que)
+
+# 다른 토마토들이 익은토마토의 영향을 받았는지 확인하여 0이 있다면 check를 1로 바꾼다
+for i in graph:
+    if 0 in i:
+        check = 1
         break
-if in_zero:
+
+# 토마토가 다 익지 않았다면 -1 출력
+if check:
     print(-1)
+# 다 익었다면 며칠만에 다익었는지 출력
 else:
-    print(answer)
+    print(days)
