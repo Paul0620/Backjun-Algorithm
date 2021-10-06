1f4c4e0a2c5709d19ea807c005d93b55e737fb04
Paul <79445646+Paul0620@users.noreply.github.com>
2021-10-06 21:17:55 +0900

    Update file

--- main.py
+++ main.py
@@ -1,44 +1,27 @@
-# 봄버맨
-from sys import stdin
-from collections import deque
+a = [[60, 50], [30, 70], [60, 30], [80, 40]]
+b = [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]
+c = [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]
 
-def search_bombs():
-    for x in range(r):
-        for y in range(c):
-            if graph[x][y] == 'O':
-                que.append((x, y))
+# 1. w, h중 한쪽은 최대값을 사용
+# 2. 나머지 하나 값에서 회전시켰을 때 그 값보다 큰지
+# 3. 크다면 그 중 가장 최소값을 찾아야함.
 
-def make_bombs():
-    for x in range(r):
-        for y in range(c):
-            if graph[x][y] == '.':
-                graph[x][y] = 'O'
+def solution(sizes):
+    answer = 0
+    w = 0
+    h = 0
+    for i in sizes:
+        i.sort()
+        if w < i[0]:
+            w = i[0]
+        if h < i[1]:
+            h = i[1]
 
-def explosion():
-    while que:
-        x, y = que.popleft()
-        graph[x][y] = '.'
-        if 0 <= x - 1:
-            graph[x - 1][y] = '.'
-        if x + 1 < r:
-            graph[x + 1][y] = '.'
-        if 0 <= y - 1:
-            graph[x][y - 1] = '.'
-        if y + 1 < c:
-            graph[x][y + 1] = '.'
+    answer = w * h
 
-r, c, n = map(int, stdin.readline().split())
-graph = [list(stdin.readline().strip()) for _ in range(r)]
-que = deque()
-n -= 1
-while n:
-    search_bombs()
-    make_bombs()
-    n -= 1
-    if n == 0:
-        break
-    explosion()
-    n -= 1
+    return answer
 
-for i in graph:
-    print(''.join(i))
+
+print(solution(a))
+print(solution(b))
+print(solution(c))
