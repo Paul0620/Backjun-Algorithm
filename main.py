55075891c1ae0ce14d97e1a52f484253f636f53d
Paul <79445646+Paul0620@users.noreply.github.com>
2021-11-07 14:07:18 +0900

    Update

--- main.py
+++ main.py
@@ -1,27 +1,56 @@
-a = [[60, 50], [30, 70], [60, 30], [80, 40]]
-b = [[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]
-c = [[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]
+from random import randint
 
-# 1. w, h중 한쪽은 최대값을 사용
-# 2. 나머지 하나 값에서 회전시켰을 때 그 값보다 큰지
-# 3. 크다면 그 중 가장 최소값을 찾아야함.
+def generate_numbers():
+    numbers = []
+    while len(numbers) < 3:
+        num = randint(0, 9)
+        if num not in numbers:
+            numbers.append(num)
 
-def solution(sizes):
-    answer = 0
-    w = 0
-    h = 0
-    for i in sizes:
-        i.sort()
-        if w < i[0]:
-            w = i[0]
-        if h < i[1]:
-            h = i[1]
-
-    answer = w * h
-
-    return answer
+    return numbers
 
 
-print(solution(a))
-print(solution(b))
-print(solution(c))
+def take_guess():
+    print("숫자 3개를 하나씩 차례대로 입력하세요.")
+
+    new_guess = []
+
+    while len(new_guess) < 3:
+        enter = int(input("{}번째 숫자를 입력하세요: ".format(len(new_guess) + 1)))
+        if 0 <= enter <= 9 and enter not in new_guess:
+            new_guess.append(enter)
+        elif enter in new_guess:
+            print("중복되는 값입니다. 다시 입력하세요")
+        else:
+            print("범위를 벗어나는 숫자입니다. 다시 입력하세요.")
+
+    return new_guess
+
+
+def get_score(guess,answer):
+    strike_count = 0
+    ball_count = 0
+
+    for i in range(3):
+        if guess[i] == answer[i]:
+            strike_count += 1
+        elif guess[i] in answer:
+            ball_count += 1
+
+    return strike_count, ball_count
+
+# 여기서부터 게임 시작!
+ANSWER = generate_numbers()
+tries = 0
+print("0과 9사이의 서로 다른 숫자x 3개를 랜덤한 순서로 뽑았습니다.\n")
+
+while True:
+    user_guess = take_guess()
+    s, b = get_score(user_guess, ANSWER)
+
+    if s == 3:
+        print("축하합니다. {}번 만에 숫자 3개의 값과 위치를 모두 맞추셨습니다.".format(tries))
+        break
+    else:
+        print("{}S {}B\n".format(s, b))
+        tries += 1
